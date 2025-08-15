dfef191f495ca4d28c7308898061dfb8998c4404
Dhanushka <dhanushkamcr@gmail.com>
2025-08-15 15:22:36 -0500

    Ddefault values for init parameters

--- class.py
+++ class.py
@@ -2,15 +2,18 @@ class Car:
     # Class Attribute
     wheels = 4
 
-    def __init__(self, make, model):
+    def __init__(self, make, model, year=2025):
         # Instance Attributes
         self.make = make
         self.model = model
+        self.year = year
 
 # Creating instances of the Car class
 car1 = Car('Toyota', 'Corolla')
-car2 = Car('Honda', 'Civic')
+car2 = Car('Honda', 'Civic', 2018)
 
 print(car1.wheels)  # Output: 4
 print(car1.make)    # Output: Toyota
 print(car2.make)    # Output: Honda
+print(car1.year)    # Output: Honda
+print(car2.year)    # Output: Honda
