db1293770fc895f451ad72d00cc78a5caba13815
Dhanushka <dhanushkamcr@gmail.com>
2025-08-17 11:23:38 -0500

    Static Methods: Utility Functions within Classes

--- oop_advanced.py
+++ oop_advanced.py
@@ -50,3 +50,32 @@ car2 = Car.from_csv("Honda Accord,2018")
 print(Car.total_produced()) # Output: 2
 print(car1.total_produced()) # Retrieve using instances
 print(car2.total_produced()) # Retrieve using instances
+print("\n=====================================\n")
+
+
+
+# Static Methods: Utility Functions within Classes
+class WeatherStation:
+    
+    def __init__(self, location, temperature_f):
+        self.location = location
+        self.temperature_f = temperature_f
+    
+    def __repr__(self):
+        return f"WeatherStation(location={self.location}, temperature_f={self.temperature_f})"
+    
+    @staticmethod
+    def fahrenheit_to_celsius(f_temp):
+        return (f_temp - 32) * 5.0/9.0
+    
+    @staticmethod
+    def celsius_to_fahrenheit(c_temp):
+        return (c_temp * 9.0/5.0) + 32
+
+# Usage of static methods:
+temp_f = 77
+temp_c = WeatherStation.fahrenheit_to_celsius(temp_f) #Notice here we didn't make any instance from the class
+print(f"{temp_f}F is {temp_c:.2f}C")  # Output: 77F is 25.00C
+
+temp_back_f = WeatherStation.celsius_to_fahrenheit(temp_c)
+print(f"{temp_c:.2f}C is {temp_back_f:.2f}F")  # Output: 25.00C is 77.00F
